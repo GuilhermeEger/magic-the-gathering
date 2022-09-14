25cb5bf58edc698d921ae3ad00038f06a3906aaf
Guilherme Eger <guilhermeeger.developer@gmail.com>
2022-09-13 21:05:38 -0300

    first commit

---
+++ MagicApp/src/pages/Home/index.js
@@ -0,0 +1,24 @@
+
+function Home() {
+    return (
+      <div className="App">
+        <div id ='header' style={{height:50, width:'100%', background:'red', position:'absolute', top:0, left:0}}>
+
+        </div>
+
+        <div id='corpo' style={{height:'100%', background:'blue'}}>
+
+            <div style={{width:50, height:50, border:'1px solid black'}}></div>
+
+
+        </div>
+
+        <div id='footer' style={{height:50, width:'100%', background:'red', position:'absolute', bottom:0, left:0}}>
+
+        </div>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
