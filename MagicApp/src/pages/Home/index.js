a004a5bb7e67573bf0c2d71f009a821cae802612
Guilherme Eger <guilhermeeger.developer@gmail.com>
2022-09-13 21:42:08 -0300

    Casca home

--- MagicApp/src/pages/Home/index.js
+++ MagicApp/src/pages/Home/index.js
@@ -1,19 +1,29 @@
+import React, { useState, useEffect } from 'react';
+const mtg = require('mtgsdk');
 
 function Home() {
+
+    useEffect(() => {
+        mtg.card.find(3)
+        .then(result => {
+            console.log(result.card.name) // "Black Lotus"
+        })
+      });
+
     return (
-      <div className="App">
-        <div id ='header' style={{height:50, width:'100%', background:'red', position:'absolute', top:0, left:0}}>
+      <div style={{height:'100%'}}>
+        <div id ='header' style={{height:50, width:'100%', background:'#53eddb',}}>
 
         </div>
 
-        <div id='corpo' style={{height:'100%', background:'blue'}}>
+        <div id='corpo' style={{height:'calc(100% - 100px)'}}>
 
-            <div style={{width:50, height:50, border:'1px solid black'}}></div>
+            <div style={{padding:10, height:50, border:'1px solid black', position:'absolute', top:'50%', left:'50%', cursor:'pointer'}}>Mostrar cartas</div>
 
 
         </div>
 
-        <div id='footer' style={{height:50, width:'100%', background:'red', position:'absolute', bottom:0, left:0}}>
+        <div id='footer' style={{height:50, width:'100%', background:'#53eddb'}}>
 
         </div>
       </div>
